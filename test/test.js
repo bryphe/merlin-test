1de15f23c0f45e4170354d78c51f7243b0dd320e
Bryan Phelps <bryphe@outlook.com>
2018-11-08 18:13:14 -0800

    Fix up tests

--- test/test.js
+++ test/test.js
@@ -21,29 +21,36 @@ let runMerlinCommand = (args, options) => {
                 input: null,
                 ...options };
 
-    let augmentedPath = path.dirname(ocamlMerlinReasonPath) + ";" + process.env.PATH;
+    let augmentedPath = path.dirname(ocamlMerlinReasonPath) + ";" + process.env["Path"];
 
-    console.log("Augmented Path: " + augmentedPath);
-    console.log("Using cwd: " + options.cwd);
+    let env = {
+        ...process.env,
+        ["Path"]: augmentedPath,
+        ["PATH"]: augmentedPath
+    };
+
+    console.log("[runMerlinCommand] Using cwd: " + options.cwd);
     return new Promise((resolve, reject) => {
         let proc = cp.spawn(ocamlMerlinPath, args, {
             cwd: options.cwd,
-            env: {
-                ...process.env,
-                PATH: augmentedPath,
-            }
+            env,
         })
-        // proc.stdin.setEncoding('utf-8');
-        // proc.stdout.pipe(process.stdout);
+
+        if (options.input) {
+            proc.stdin.setEncoding('utf-8');
+            proc.stdin.write(options.input);
+            proc.stdin.end();
+        }
 
         let data = "";
         let err = "";
         proc.stdout.on("data", (d) => {
-            console.log("DATA: " + d);
+            console.log("[Merlin - output]: " + d);
             data += d;
         })
 
         proc.stderr.on("data", (d) => {
+            console.log("[Merlin - error]: " + d);
             err += d;
         })
 
@@ -67,21 +74,44 @@ let runMerlinCommand = (args, options) => {
 };
 
 describe("type-enclosing", () => {
+
+    let input = `
+let x = 1
+let y = .5
+    `;
+
     test("Can give me a type from a .ml file", async () => {
-        let { data }  = await runMerlinCommand(["single", "type-enclosing", "-position", "1:5", "-filename bin/test.ml"], {
-            cwd: root
+        let { data }  = await runMerlinCommand(["single", "type-enclosing", "-position", "2:5", "-filename bin/test.ml"], {
+            cwd: root,
+            input
         });
         console.dir(data);
         expect(data.class).toBe('return');
     });
 
-    test("Can give me a type from a .re file", async () => {
+let reasonInput = `let x = 1;`
+    test("Can give me a type from a single-line .re file", async () => {
         let { data }  = await runMerlinCommand(["single", "type-enclosing", "-position", "1:5", "-filename bin/Hello.re"], {
-            cwd: root
+            cwd: root,
+            input: reasonInput
         });
         console.dir(data);
         expect(data.class).toBe('return');
     });
+
+let reasonMultiLineInput = `
+let x = 1;
+let y = 2;
+`
+    test("Can give me a type from a multi-line .re file", async () => {
+        let { data }  = await runMerlinCommand(["single", "type-enclosing", "-position", "2:5", "-filename bin/Hello.re"], {
+            cwd: root,
+            input: reasonMultiLineInput
+        });
+        console.dir(data);
+        expect(data.class).toBe('return');
+    });
+
 })
 
 describe("path-of-source", () => {
